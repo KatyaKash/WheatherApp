16b080746808d969eb915db4e26314f46b8d9f2c
Blina <katya_kash@inbox.ru>
2023-01-16 10:56:08 +0700

    Добавьте файлы проекта.

---
+++ WheatherApp/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WheatherApp
+{
+    internal interface Subject
+    {
+        public void registerObserver(Observer o);
+        public void removeObserver(Observer o);
+        public void notifyObservers();
+
+    }
+}
